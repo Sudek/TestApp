c7ca04d7a4999b638f649e4a231f861f33acb947
Andrey Sviridov <andrey.sviridov4@gmail.com>
2016-10-22 11:57:45 +0300

    init commit

---
+++ app/src/main/java/ikko_ikki/testapp/DateDialog.java
@@ -0,0 +1,13 @@
+package ikko_ikki.testapp;
+
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.widget.DatePicker;
+
+public class DateDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+    }
+}
